5e0c135660029b5497ae0ed4a554000c6aa3844a
Saussereau Dominique <domsau2@yahoo.fr>
2018-08-07 22:02:59 +0200

    + file.

--- main.cpp
+++ main.cpp
@@ -1,19 +1,22 @@
 #include <iostream>
+#include <fstream>
 #include <stdlib.h>     /* srand, rand */
-#include <time.h>      
-
+#include <time.h>
+#include <string>
 using namespace std;
 
 int main()
 {
     srand (time(NULL));
-    cout << "Ceci donne un mot au pseudo hasard (lisible, donc plus facile à retenir)." << endl;
-    cout << "Cela peut être utile pour un mot de passe, un captcha..." << endl;
+    cout << "Ceci donne un mot au pseudo hasard." << endl;
+    cout << "C\'est lisible, donc c\'est plus facile de le retenir ou de le recopier." << endl;
+    cout << "C\'est utile pour un mot de passe, un captcha..." << endl;
     cout << "Certaines lettres ne seront pas inscrites, volontairement, " << endl;
     cout << "pour limiter les incertitudes." << endl << endl;
     string tentativeString = "";
     bool inputOk = true;
     int longueur = 0;
+    string mot = "";
     int valLettreVoyelle = 0;
     int valLettreConsonne = 0;
     int minVoyelle = 0;
@@ -42,22 +45,38 @@ int main()
         }
     }
     while (inputOk == false);
-
-
     for (int a = 0; a < longueur; a++)
     {
-        valLettreConsonne = rand() % maxConsonne + minConsonne; // -1 ?
-        cout << consonnes[valLettreConsonne];
-
+        valLettreConsonne = rand() % maxConsonne + minConsonne;
+        mot = mot + consonnes[valLettreConsonne];
         a++; // Deux lettres par boucle.
-if (a < longueur) // Pour éviter les dépassements.
-{
-
-        valLettreVoyelle = rand() % maxVoyelle + minVoyelle; // -1 ?
-        cout << voyelles[valLettreVoyelle];
-}
-
+        if (a < longueur) // Pour éviter les dépassements.
+        {
+            valLettreVoyelle = rand() % maxVoyelle + minVoyelle;
+            mot = mot + voyelles[valLettreVoyelle];
+        }
     }
-
+    cout << mot << endl;
+    string nomFichier = "";
+    cout << endl;
+    cout << "Si vous voulez sauvegarger cette signature, donnez un nom de fichier, " << endl;
+    cout << "avec son extension." << endl;
+    cout << "Si vous donnez le nom d'un fichier existant, il sera mis sur la ligne suivante." << endl;
+    cout << "Si vous annulez, il n\'y aura aucune sauvegarde !" << endl;
+    getline(cin, nomFichier);
+    if (nomFichier !="")
+    {
+        ofstream myFile(nomFichier.c_str(), ios::app); // Open to append, ".niv" included.
+        if (!myFile)
+        {
+            cerr << "Erreur de fichier !";
+        }
+        else
+        {
+            myFile << mot << endl;// append file.
+        }
+        myFile.close();
+    }
+    cout << "OK." << endl;
     return 0;
 }
